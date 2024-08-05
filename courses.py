d62eff2ff1b374840ddfa9e87fbc664aa2e0209b
Jan Derick Velez <jderickvelez@gmail.com>
2024-08-05 15:37:45 +0800

    Added Course table

--- courses.py
+++ courses.py
@@ -1,38 +1,38 @@
 from database_manager import DatabaseManager
 
 
-class Departments(DatabaseManager):
+class Courses(DatabaseManager):
         
-    def create_department(self, department_name,department_code):
-        query = "INSERT INTO Departments( department_name, department_code) VALUES ( %s,%s)"
-        parameter = ( department_name, department_code)
+    def create_courses(self, course,course_type):
+        query = "INSERT INTO Courses( course, course_type) VALUES ( %s,%s)"
+        parameter = ( course, course_type)
         self.execute_command(query,parameter)
     
-    def read_department(self):
-        query = "SELECT * FROM Departments"
+    def read_courses(self):
+        query = "SELECT * FROM Courses"
         self.execute_query(query)
     
-    def update_department(self, department_name, department_id):
-        query = "UPDATE Departments SET department_name =  %s  WHERE department_id = %s"
-        parameter = (department_name, department_id)
+    def update_courses(self, course, course_type, courses_id):
+        query = "UPDATE Courses SET course =  %s, course_type = %s  WHERE course_id = %s"
+        parameter = (course, course_type, courses_id)
+        self.execute_command(query,parameter)
+         
+    def delete_courses(self, course_id):
+        query = "DELETE FROM Courses WHERE course_id = %s"
+        parameter = str(course_id)
         self.execute_command(query,parameter)
         
-    def delete_department(self, department_id):
-        query = "DELETE FROM Departments WHERE department_id = %s"
-        parameter = str(department_id)
-        self.execute_command(query,parameter)
-        
-    def get_by_id(self, department_id ):
-        query = "SELECT * FROM Departments WHERE department_id = %s"
-        parameter = str(department_id)
+    def get_by_id(self, course_id ):
+        query = "SELECT * FROM Courses WHERE course_id = %s"
+        parameter = str(course_id)
         single = True
         self.execute_query(query,parameter,single)
         
-ad = Departments()
+ad = Courses()
 
-# ad.create_department('dsafa', 'sdsd')
-# ad.update_department('hello',2)
-ad.delete_department('1')
-ad.read_department()
+# ad.create_courses('dsafa', 'sdsd')
+ad.update_courses('sdfs', 'hello',2)
+# ad.delete_courses('1')
+# ad.read_courses()
 # ad.get_by_id('2') 
 # # print(ad.get_by_id('5') )
